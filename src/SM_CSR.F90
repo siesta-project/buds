5b943177ce6a3b1a1cb4593a0c2229148a14b958
Nick Papior <nickpapior@gmail.com>
2016-02-20 00:40:00 +0100

    Added preliminary CSR sparse matrix
    - This matrix format is the most basic implementation
      of the MKL SparseBLAS format using the:
        - 1-based indexing
        - 3-array variant for least memory usage and the
          easiest interaction.
    
      There are routines for querying specific indices
      for the sparse elements.
    
      The data format should be revised to see if there
      may be issues/improvements.
    
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

---
+++ src/SM_CSR.F90
@@ -0,0 +1,44 @@
+  ! @@LICENSE@@ see Copyright notice in the top-directory
+
+! Get default commands
+#include "refype_utils.inc"
+
+! Integer (int)
+# define _R_MOD_NAME _R_CC3(_R_MOD,_,iSM_CSR)
+!> @file _R_MOD_NAME
+module _R_MOD_NAME
+# define _R_TYPE_NAME _R_CC3(_R_TYPE,_,iSM_CSR)
+# define _R_TYPE_NEW _R_CC3(_R_NEW,_,iSM_CSR)
+# define _R_TYPE_VAR integer
+# define _R_TYPE_VAR_PREC ii_
+#include "SM_CSR.inc"
+end module
+
+! Integer (long)
+# define _R_MOD_NAME _R_CC3(_R_MOD,_,lSM_CSR)
+!> @file _R_MOD_NAME
+module _R_MOD_NAME
+# define _R_TYPE_NAME _R_CC3(_R_TYPE,_,lSM_CSR)
+# define _R_TYPE_NEW _R_CC3(_R_NEW,_,lSM_CSR)
+# define _R_TYPE_VAR integer
+# define _R_TYPE_VAR_PREC il_
+#include "SM_CSR.inc"
+end module
+
+
+! project-refype -- local file settings
+!     Anything below this line may be overwritten by scripts
+!     Below are non-editable settings
+
+! Local Variables:
+!  mode: f90
+!  f90-if-indent: 2
+!  f90-type-indent: 2
+!  f90-associate-indent: 2
+!  f90-continuation-indent: 2
+!  f90-structure-indent: 2
+!  f90-critical-indent: 2
+!  f90-program-indent: 2
+!  f90-do-indent: 2
+! End:
+
