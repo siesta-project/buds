9c5c0bdac0100ce09f3b7fae394b4213a7fd7b48
Nick Papior <nickpapior@gmail.com>
2016-02-21 10:57:39 +0100

    Fixed ArraynD to actually be nD
    - Now the copy is made by the Makefile
    
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- src/dublicate.sh
+++ src/dublicate.sh
@@ -3,7 +3,7 @@
 # This small script dublicates certain files
 # in case small changes has been done.
 
-for dat in Array1D FS_Array1D C2_Array1D FS_C2_Array1D
+for dat in FS_Array1D C2_Array1D FS_C2_Array1D
 do
     # basic file
     f=mryp_${dat}.F90
