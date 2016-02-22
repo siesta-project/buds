0c719b8e7ed37dca2f867cfe389fc2078231ebc3
Nick Papior <nickpapior@gmail.com>
2016-02-22 10:49:44 +0100

    bug: forgot to add test_utils
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

---
+++ test/test_utils.F90
@@ -0,0 +1,37 @@
+module test_utils
+
+# include "refype_common_declarations.inc"
+
+  public :: tic, toc
+
+contains
+
+  function tic()
+    real :: tic
+    call cpu_time(tic)
+  end function tic
+
+  function toc()
+    real :: toc
+    call cpu_time(toc)
+  end function toc
+
+end module test_utils
+
+
+! project-refype -- local file settings
+!     Anything below this line may be overwritten by scripts
+!     Below are non-editable settings
+
+! Local Variables:
+!  mode: f90
+!  f90-if-indent: 2
+!  f90-type-indent: 2
+!  f90-associate-indent: 2
+!  f90-continuation-indent: 2
+!  f90-structure-indent: 2
+!  f90-critical-indent: 2
+!  f90-program-indent: 2
+!  f90-do-indent: 2
+! End:
+
