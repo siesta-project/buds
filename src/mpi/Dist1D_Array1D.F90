ed3ecb8426a3e1c599fa5540dc61898bb053506b
Nick Papior <nickpapior@gmail.com>
2016-12-02 13:20:21 +0100

    enh: added the distributed arrays, added a test
    This fixes #6 by implementing the distributed arrays.
    Its use is exactly equivalent to the distributed sparse
    matrix but does work on dense arrays.
    Added a test which also works.
    
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

---
+++ src/mpi/Dist1D_Array1D.F90
@@ -0,0 +1,159 @@
+  ! @@LICENSE@@ see Copyright notice in the top-directory
+
+! Get default commands
+#include "bud_utils.inc"
+
+!> @defgroup dist1d-array Dense distributed array
+!!
+!! @{
+
+
+! Define the number of dimensions in this Array
+#define BUD_DIM 1
+#define BUD_DIMD BUD_CC2(BUD_DIM,D)
+
+
+#if BUD_DIM == 1
+!> @defgroup dist1d-array-BUD_DIM Distributed dense array
+#elif BUD_DIM == 2
+!> @defgroup dist1d-array-BUD_DIM Distributed dense array, 1 extra dimension per element
+#elif BUD_DIM == 3
+!> @defgroup dist1d-array-BUD_DIM Distributed dense array, 2 extra dimensions per element
+#elif BUD_DIM == 4
+!> @defgroup dist1d-array-BUD_DIM Distributed dense array, 3 extra dimensions per element
+#elif BUD_DIM == 5
+!> @defgroup dist1d-array-BUD_DIM Distributed dense array, 4 extra dimensions per element
+#endif
+!! @ingroup dist1d-BUD_DIM
+!!
+!! A distributed dense array.
+!!
+!! This @bud is fully self-contained and enables the interaction
+!! with a dense array and its data.
+!!
+!! @ref array-BUD_DIM is the used container.
+!!
+!! @{
+
+#define BUD_PREC ii_
+
+# define BUD_MOD_NAME BUD_CC3(BUD_MOD,_,iDist1D_b1D)
+!> @defgroup BUD_MOD_NAME Logical
+!! `logical` data type
+!!
+!! Distributed dense array with logical content.
+!! @{
+module BUD_MOD_NAME
+  use BUD_CC3(BUD_MOD,_,iDist1D)
+  use BUD_CC3(BUD_MOD,_,bArray1D)
+# define BUD_TYPE_NAME BUD_CC2(BUD_TYPE,iD1D_b1D)
+# define BUD_TYPE_NEW BUD_CC3(BUD_NEW,_,D1D_1D)
+# define BUD_TYPE_VAR logical
+#undef BUD_TYPE_VAR_PREC
+# define BUD_COLL_1 BUD_CC2(BUD_TYPE,iDist1D)
+# define BUD_COLL_2 BUD_CC2(BUD_TYPE,bArray1D)
+#include "Dist1D_Array.inc"
+end module
+!> @}
+
+# define BUD_MOD_NAME BUD_CC3(BUD_MOD,_,iDist1D_r1D)
+!> @defgroup BUD_MOD_NAME Real (float)
+!! `real(selected_real_kind(6)` data type
+!!
+!! Distributed dense array with single precision.
+!! @{
+module BUD_MOD_NAME
+  use BUD_CC3(BUD_MOD,_,iDist1D)
+  use BUD_CC3(BUD_MOD,_,rArray1D)
+# define BUD_TYPE_NAME BUD_CC2(BUD_TYPE,iD1D_r1D)
+# define BUD_TYPE_NEW BUD_CC3(BUD_NEW,_,D1D_1D)
+# define BUD_TYPE_VAR real
+# define BUD_TYPE_VAR_PREC rr_
+# define BUD_COLL_1 BUD_CC2(BUD_TYPE,iDist1D)
+# define BUD_COLL_2 BUD_CC2(BUD_TYPE,rArray1D)
+#include "Dist1D_Array.inc"
+end module
+!> @}
+
+# define BUD_MOD_NAME BUD_CC3(BUD_MOD,_,iDist1D_d1D)
+!> @defgroup BUD_MOD_NAME Real (double)
+!! `real(selected_real_kind(15)` data type
+!!
+!! Distributed dense array with double precision.
+!! @{
+module BUD_MOD_NAME
+  use BUD_CC3(BUD_MOD,_,iDist1D)
+  use BUD_CC3(BUD_MOD,_,dArray1D)
+# define BUD_TYPE_NAME BUD_CC2(BUD_TYPE,iD1D_d1D)
+# define BUD_TYPE_NEW BUD_CC3(BUD_NEW,_,D1D_1D)
+# define BUD_TYPE_VAR real
+# define BUD_TYPE_VAR_PREC rd_
+# define BUD_COLL_1 BUD_CC2(BUD_TYPE,iDist1D)
+# define BUD_COLL_2 BUD_CC2(BUD_TYPE,dArray1D)
+#include "Dist1D_Array.inc"
+end module
+!> @}
+
+# define BUD_MOD_NAME BUD_CC3(BUD_MOD,_,iDist1D_c1D)
+!> @defgroup BUD_MOD_NAME Complex (float)
+!! `complex(selected_real_kind(6)` data type
+!!
+!! Distributed dense array with complex single precision.
+!! @{
+module BUD_MOD_NAME
+  use BUD_CC3(BUD_MOD,_,iDist1D)
+  use BUD_CC3(BUD_MOD,_,cArray1D)
+# define BUD_TYPE_NAME BUD_CC2(BUD_TYPE,iD1D_c1D)
+# define BUD_TYPE_NEW BUD_CC3(BUD_NEW,_,D1D_1D)
+# define BUD_TYPE_VAR complex
+# define BUD_TYPE_VAR_PREC rr_
+# define BUD_COLL_1 BUD_CC2(BUD_TYPE,iDist1D)
+# define BUD_COLL_2 BUD_CC2(BUD_TYPE,cArray1D)
+#include "Dist1D_Array.inc"
+end module
+!> @}
+
+# define BUD_MOD_NAME BUD_CC3(BUD_MOD,_,iDist1D_z1D)
+!> @defgroup BUD_MOD_NAME Complex (double)
+!! `complex(selected_real_kind(15)` data type
+!!
+!! Distributed dense array with complex double precision.
+!! @{
+module BUD_MOD_NAME
+  use BUD_CC3(BUD_MOD,_,iDist1D)
+  use BUD_CC3(BUD_MOD,_,zArray1D)
+# define BUD_TYPE_NAME BUD_CC2(BUD_TYPE,iD1D_z1D)
+# define BUD_TYPE_NEW BUD_CC3(BUD_NEW,_,D1D_1D)
+# define BUD_TYPE_VAR complex
+# define BUD_TYPE_VAR_PREC rd_
+# define BUD_COLL_1 BUD_CC2(BUD_TYPE,iDist1D)
+# define BUD_COLL_2 BUD_CC2(BUD_TYPE,zArray1D)
+#include "Dist1D_Array.inc"
+end module
+!> @}
+
+
+
+! GROUP dist1d-array-BUD_DIM
+!> @}
+
+! GROUP dist1d-array
+!> @}
+
+
+! project-bud -- local file settings
+!     Anything below this line may be overwritten by scripts
+!     Below are non-editable settings
+
+! Local Variables:
+!  mode: f90
+!  f90-if-indent: 2
+!  f90-type-indent: 2
+!  f90-associate-indent: 2
+!  f90-continuation-indent: 2
+!  f90-structure-indent: 2
+!  f90-critical-indent: 2
+!  f90-program-indent: 2
+!  f90-do-indent: 2
+! End:
+
---
+++ src/mpi/test/dist_array.F90
@@ -0,0 +1,314 @@
+program dist
+
+  ! we use everything
+  use mpi
+
+  use bud_Dist_common
+  use bud_MP_Comm
+  use bud_iDist1D
+  use bud_rArray1D
+  use bud_iDist1D_r1D
+
+  implicit none
+
+  integer, parameter :: N = 21
+  integer, parameter :: BS = 2
+
+  integer :: rank, nrank
+
+  ! Distributions
+  type(MP_Comm) :: world, comm1, self
+  type(iDist1D) :: d1, d2, d3
+  type(rArray1D) :: arr1, arr2, arr3
+  type(iD1D_r1D) :: dA1, dA2, dA3
+
+  integer :: err
+
+  call MPI_Init(err)
+
+  ! This is a duplicate of the world communicator.
+  call new(world, MPI_Comm_World)
+  rank = comm_rank(world)
+  nrank = comm_size(world)
+
+  ! Also create a duplicate of the self communicator
+  call new(self, MPI_Comm_Self)
+
+  call print_world_self()
+  call dist_1()
+  call print_world_self()
+  call dist_2()
+  call print_world_self()
+  call dist_3()
+  call print_world_self()
+  call dist_4()
+  call print_world_self()
+
+  call delete(self)
+  call delete(world)
+
+  call print_world_self()
+
+  call MPI_Finalize(err)
+
+contains
+
+  subroutine print_world_self()
+
+    if ( rank == 0 ) then
+       call print(self)
+       call print(world)
+       call flush(6)
+    end if
+
+    if ( is_initd(world) ) &
+         call Barrier(world)
+
+  end subroutine print_world_self
+
+
+  subroutine dist_1()
+    integer, pointer :: ptr(:), ind(:), ncol(:)
+    real, pointer :: arr(:)
+    integer :: ic, i, idx
+    print *,'dist-arr_1 -- start'
+
+    if ( comm_rank(world) == 0 ) then
+
+       ! Create the distribution
+       call new(d1, self, BS, N, &
+            DIST_BLOCK_CYCLIC_FIRST_SPLIT)
+
+       call new(arr1, (/N * 3/) )
+       arr => array_p(arr1)
+
+       ! Create the input distribution
+       call new(dA1, d1, arr1)
+
+       ! clean-up
+       call delete(d1)
+       call delete(arr1)
+
+    end if
+
+    ! Create the distribution
+    call new(d1, self, BS, N, &
+         DIST_BLOCK_CYCLIC_FIRST_SPLIT)
+
+    ! Create all the output distributions
+    call distribute(dA1, world, d1, dA2)
+    call delete(d1)
+    do i = 0 , nrank - 1
+       if ( i == rank ) &
+            call print(dA1)
+       call flush(6)
+       call Barrier(world)
+    end do
+    do i = 0 , nrank - 1
+       if ( i == rank ) &
+            call print(dA2)
+       call flush(6)
+       call Barrier(world)
+    end do
+
+    call delete(dA1)
+    call delete(dA2)
+
+    print *,'dist-arr_1 -- end', rank
+
+    call Barrier(world)
+
+  end subroutine dist_1
+
+
+  subroutine dist_2()
+    integer, pointer :: ptr(:), ind(:), ncol(:)
+    real, pointer :: arr(:)
+    integer :: ic, i, idx
+    print *,'dist-arr_2 -- start'
+
+    if ( comm_rank(world) == 0 ) then
+
+       ! Create the distribution
+       call new(d1, self, BS, N, &
+            DIST_BLOCK_CYCLIC_FIRST_SPLIT)
+
+       call new(arr1, N * 3)
+       arr => array_p(arr1)
+
+       ! Create the input distribution
+       call new(dA1, d1, arr1)
+
+       ! clean-up
+       call delete(d1)
+       call delete(arr1)
+
+    end if
+
+    call Comm_Split(world, mod(rank, 2), rank, comm1)
+
+    ! Create the distribution
+    call new(d1, comm1, BS, N, &
+         DIST_BLOCK_CYCLIC_FIRST_SPLIT)
+    call delete(comm1)
+
+    ! Create all the output distributions
+    call distribute(dA1, world, d1, dA2)
+    call delete(d1)
+    do i = 0 , nrank - 1
+       if ( i == rank ) &
+            call print(dA1)
+       call flush(6)
+       call Barrier(world)
+    end do
+    do i = 0 , nrank - 1
+       if ( i == rank ) &
+            call print(dA2)
+       call flush(6)
+       call Barrier(world)
+    end do
+
+    call delete(dA1)
+    call delete(dA2)
+
+    print *,'dist-arr_2 -- end', rank
+
+    call Barrier(world)
+
+  end subroutine dist_2
+
+  subroutine dist_3()
+    integer, pointer :: ptr(:), ind(:), ncol(:)
+    real, pointer :: arr(:)
+    integer :: ic, i, idx
+    print *,'dist-arr_3 -- start'
+
+    if ( comm_rank(world) == 0 ) then
+
+       ! Create the distribution
+       call new(d1, self, BS, N, &
+            DIST_BLOCK_CYCLIC_FIRST_SPLIT)
+
+       call new(arr1, N * 3)
+       arr => array_p(arr1)
+
+       ! Create the input distribution
+       call new(dA1, d1, arr1)
+
+       ! clean-up
+       call delete(d1)
+       call delete(arr1)
+
+    end if
+
+    call Comm_Split(world, mod(rank, 3), rank, comm1)
+
+    ! Create the distribution
+    call new(d1, comm1, BS, N, &
+         DIST_BLOCK_CYCLIC_FIRST_SPLIT)
+    call delete(comm1)
+
+    ! Create all the output distributions
+    call distribute(dA1, world, d1, dA2)
+    call delete(d1)
+    do i = 0 , nrank - 1
+       if ( i == rank ) &
+            call print(dA1)
+       call flush(6)
+       call Barrier(world)
+    end do
+    do i = 0 , nrank - 1
+       if ( i == rank ) &
+            call print(dA2)
+       call flush(6)
+       call Barrier(world)
+    end do
+
+    call delete(dA1)
+    call delete(dA2)
+
+    print *,'dist-arr_3 -- end', rank
+
+    call Barrier(world)
+
+  end subroutine dist_3
+
+  subroutine dist_4()
+    integer, pointer :: ptr(:), ind(:), ncol(:)
+    real, pointer :: arr(:)
+    integer :: ic, i, idx
+    print *,'dist-arr_4 -- start'
+
+    if ( comm_rank(world) == 0 ) then
+
+       ! Create the distribution
+       call new(d1, self, BS, N, &
+            DIST_BLOCK_CYCLIC_FIRST_SPLIT)
+
+       call new(arr1, N * 3)
+       arr => array_p(arr1)
+
+       ! Create the input distribution
+       call new(dA1, d1, arr1)
+
+       ! clean-up
+       call delete(d1)
+       call delete(arr1)
+
+    end if
+
+    call Comm_Split(world, 0, rank, comm1)
+
+    ! Create the distribution
+    call new(d1, comm1, N, DIST_BLOCK_LAST_ALL)
+    call delete(comm1)
+
+    ! Create all the output distributions
+    call distribute(dA1, world, d1, dA2)
+    call delete(d1)
+
+    do i = 0 , nrank - 1
+       if ( i == rank ) &
+            call print(dA1)
+       call flush(6)
+       call Barrier(world)
+    end do
+    do i = 0 , nrank - 1
+       if ( i == rank ) &
+            call print(dA2)
+       call flush(6)
+       call Barrier(world)
+    end do
+
+    ! Now dsmA1 is on rank == 0
+    !     dsmA2 is distributed on all ranks
+    ! Lets try and recreate dsmA1 by creating
+    ! only the output distribution on rank == 0
+    if ( rank == 0 ) then
+       ! Create the distribution
+       call new(d1, self, BS, N, &
+            DIST_BLOCK_CYCLIC_FIRST_SPLIT)
+    end if
+
+    ! Create all the output distributions
+    call distribute(dA2, world, d1, dA3)
+    call delete(d1)
+
+    do i = 0 , nrank - 1
+       if ( i == rank ) &
+            call print(dA3)
+       call flush(6)
+       call Barrier(world)
+    end do
+
+    call delete(dA1)
+    call delete(dA2)
+    call delete(dA3)
+
+    print *,'dist-arr_4 -- end', rank
+
+    call Barrier(world)
+
+  end subroutine dist_4
+
+end program dist
