20c6417ca92463172e30ee9121650bcfc1eb2362
Nick Papior <nickpapior@gmail.com>
2017-01-19 14:48:35 +0100

    doc: updated Array for FORD
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

---
+++ src/Array.F90
@@ -0,0 +1,67 @@
+  ! @@LICENSE@@ see Copyright notice in the top-directory
+
+! Get default commands
+#include "bud_utils.inc"
+
+
+module BUD_CC3(BUD_MOD,_,Array1D)
+
+  use BUD_CC3(BUD_MOD,_,sArray1D)
+  use BUD_CC3(BUD_MOD,_,iArray1D)
+  use BUD_CC3(BUD_MOD,_,lArray1D)
+  use BUD_CC3(BUD_MOD,_,rArray1D)
+  use BUD_CC3(BUD_MOD,_,dArray1D)
+  use BUD_CC3(BUD_MOD,_,cArray1D)
+  use BUD_CC3(BUD_MOD,_,zArray1D)
+
+end module
+
+module BUD_CC3(BUD_MOD,_,Array2D)
+
+  use BUD_CC3(BUD_MOD,_,sArray2D)
+  use BUD_CC3(BUD_MOD,_,iArray2D)
+  use BUD_CC3(BUD_MOD,_,lArray2D)
+  use BUD_CC3(BUD_MOD,_,rArray2D)
+  use BUD_CC3(BUD_MOD,_,dArray2D)
+  use BUD_CC3(BUD_MOD,_,cArray2D)
+  use BUD_CC3(BUD_MOD,_,zArray2D)
+
+end module
+
+module BUD_CC3(BUD_MOD,_,Array3D)
+
+  use BUD_CC3(BUD_MOD,_,sArray3D)
+  use BUD_CC3(BUD_MOD,_,iArray3D)
+  use BUD_CC3(BUD_MOD,_,lArray3D)
+  use BUD_CC3(BUD_MOD,_,rArray3D)
+  use BUD_CC3(BUD_MOD,_,dArray3D)
+  use BUD_CC3(BUD_MOD,_,cArray3D)
+  use BUD_CC3(BUD_MOD,_,zArray3D)
+
+end module
+
+module BUD_CC3(BUD_MOD,_,Array)
+
+  use BUD_CC3(BUD_MOD,_,Array1D)
+  use BUD_CC3(BUD_MOD,_,Array2D)
+  use BUD_CC3(BUD_MOD,_,Array3D)
+
+end module
+
+
+! project-buds -- local file settings
+!     Anything below this line may be overwritten by scripts
+!     Below are non-editable settings
+
+! Local Variables:
+!  mode: f90
+!  f90-if-indent: 2
+!  f90-type-indent: 2
+!  f90-associate-indent: 2
+!  f90-continuation-indent: 2
+!  f90-structure-indent: 2
+!  f90-critical-indent: 2
+!  f90-program-indent: 2
+!  f90-do-indent: 2
+! End:
+
--- src/Array1D.F90
+++ src/Array1D.F90
@@ -3,146 +3,77 @@
 ! Get default commands
 #include "bud_utils.inc"
 
-!> @defgroup array Array
-!! @ingroup bud-intrinsic
-!!
-!! Intrinsic fortran array data-type.
-!!
-!! The contained array may be of various data types.
-!! Each data type may be deciphered through this list of
-!! prefixes:
-!!   - `logical` (`b`)
-!!   - `integer(selected_int_kind(4))` (`s`), short
-!!   - `integer(selected_int_kind(9))` (`i`), int
-!!   - `integer(selected_int_kind(18))` (`l`), long
-!!   - `real(selected_real_kind(6))` (`r), float
-!!   - `real(selected_real_kind(15))` (`d`), double
-!!   - `complex(selected_real_kind(6))` (`c`), float complex
-!!   - `complex(selected_real_kind(15))` (`z`), double complex
-!!
-!! It does not contain anything but the intrinsic array.
-!!
-
 
 ! Define the number of dimensions in this Array
 #define BUD_DIM 1
 #define BUD_DIMD BUD_CC2(BUD_DIM,D)
 
-!> @defgroup array-BUD_DIM Array BUD_DIMD
-!! @ingroup array
-!! Array of dimension BUD_DIM.
-!! @{
-
 
 # define BUD_MOD_NAME BUD_CC3(BUD_MOD,_,bArray1D)
-!> @defgroup BUD_MOD_NAME Logical
-!! `logical` data type
-!! @{
 module BUD_MOD_NAME
 # define BUD_TYPE_NAME BUD_CC2(BUD_TYPE,bArray1D)
-# define BUD_TYPE_NEW BUD_CC3(BUD_NEW,_,Array)
 # define BUD_TYPE_VAR logical
 #undef BUD_TYPE_VAR_PREC
 #include "Array.inc"
 end module
-!> @}
 
 # define BUD_MOD_NAME BUD_CC3(BUD_MOD,_,sArray1D)
-!> @defgroup BUD_MOD_NAME Integer (short)
-!! `integer(selected_int_kind(4))` data type
-!! @{
 module BUD_MOD_NAME
 # define BUD_TYPE_NAME BUD_CC2(BUD_TYPE,sArray1D)
-# define BUD_TYPE_NEW BUD_CC3(BUD_NEW,_,Array)
 # define BUD_TYPE_VAR integer
 # define BUD_TYPE_VAR_PREC is_
 #include "Array.inc"
 end module
-!> @}
 
 # define BUD_MOD_NAME BUD_CC3(BUD_MOD,_,iArray1D)
-!> @defgroup BUD_MOD_NAME Integer (int)
-!! `integer(selected_int_kind(9))` data type
-!! @{
 module BUD_MOD_NAME
 # define BUD_TYPE_NAME BUD_CC2(BUD_TYPE,iArray1D)
-# define BUD_TYPE_NEW BUD_CC3(BUD_NEW,_,Array)
 # define BUD_TYPE_VAR integer
 # define BUD_TYPE_VAR_PREC ii_
 #include "Array.inc"
 end module
-!> @}
 
 # define BUD_MOD_NAME BUD_CC3(BUD_MOD,_,lArray1D)
-!> @defgroup BUD_MOD_NAME Integer (long)
-!! `integer(selected_int_kind(18))` data type
-!! @{
 module BUD_MOD_NAME
 # define BUD_TYPE_NAME BUD_CC2(BUD_TYPE,lArray1D)
-# define BUD_TYPE_NEW BUD_CC3(BUD_NEW,_,Array)
 # define BUD_TYPE_VAR integer
 # define BUD_TYPE_VAR_PREC il_
 #include "Array.inc"
 end module
-!> @}
 
 # define BUD_MOD_NAME BUD_CC3(BUD_MOD,_,rArray1D)
-!> @defgroup BUD_MOD_NAME Real (float)
-!! `real(selected_real_kind(6))` data type
-!! @{
 module BUD_MOD_NAME
 # define BUD_TYPE_NAME BUD_CC2(BUD_TYPE,rArray1D)
-# define BUD_TYPE_NEW BUD_CC3(BUD_NEW,_,Array)
 # define BUD_TYPE_VAR real
 # define BUD_TYPE_VAR_PREC rr_
 #include "Array.inc"
 end module
-!> @}
 
 # define BUD_MOD_NAME BUD_CC3(BUD_MOD,_,dArray1D)
-!> @defgroup BUD_MOD_NAME Real (double)
-!! `real(selected_real_kind(15))` data type
-!! @{
 module BUD_MOD_NAME
 # define BUD_TYPE_NAME BUD_CC2(BUD_TYPE,dArray1D)
-# define BUD_TYPE_NEW BUD_CC3(BUD_NEW,_,Array)
 # define BUD_TYPE_VAR real
 # define BUD_TYPE_VAR_PREC rd_
 #include "Array.inc"
 end module
-!> @}
 
 # define BUD_MOD_NAME BUD_CC3(BUD_MOD,_,cArray1D)
-!> @defgroup BUD_MOD_NAME Complex (float)
-!! `complex(selected_real_kind(6))` data type
-!! @{
 module BUD_MOD_NAME
 # define BUD_TYPE_NAME BUD_CC2(BUD_TYPE,cArray1D)
-# define BUD_TYPE_NEW BUD_CC3(BUD_NEW,_,Array)
 # define BUD_TYPE_VAR complex
 # define BUD_TYPE_VAR_PREC rr_
 #include "Array.inc"
 end module
-!> @}
 
 # define BUD_MOD_NAME BUD_CC3(BUD_MOD,_,zArray1D)
-!> @defgroup BUD_MOD_NAME Complex (double)
-!! `complex(selected_real_kind(15))` data type
-!! @{
 module BUD_MOD_NAME
 # define BUD_TYPE_NAME BUD_CC2(BUD_TYPE,zArray1D)
-# define BUD_TYPE_NEW BUD_CC3(BUD_NEW,_,Array)
 # define BUD_TYPE_VAR complex
 # define BUD_TYPE_VAR_PREC rd_
 #include "Array.inc"
 end module
-!> @}
 
 
-!> @defgroup Array module for all different precision objects
-!! @ingroup array-BUD_DIM
-!! Module for making everything public
-!! @{
 module BUD_CC3(BUD_MOD,_,Array1D)
 
   use BUD_CC3(BUD_MOD,_,sArray1D)
@@ -154,11 +85,6 @@ module BUD_CC3(BUD_MOD,_,Array1D)
   use BUD_CC3(BUD_MOD,_,zArray1D)
 
 end module
-!> @}
-
-
-! GROUP array-BUD_DIM
-!> @}
 
 
 ! project-buds -- local file settings
